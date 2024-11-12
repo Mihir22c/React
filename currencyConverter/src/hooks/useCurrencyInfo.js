7ebca0e4b485c5b92acf1953fc27475a5c79738d
Mihir Bavisi <mihirbavishi111@gmail.com>
2024-11-12 16:15:29 +0100

    06 currency converter project completion.

---
+++ currencyConverter/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+
+function useCurrencyInfo(currency) {
+    const [data, setData] = useState({})
+    useEffect(() => {
+        let url = `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`
+        fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                setData(data[currency])
+            })
+        console.log(data);
+    }, [currency])
+    console.log(data);
+    return data
+}
+
+export default useCurrencyInfo
